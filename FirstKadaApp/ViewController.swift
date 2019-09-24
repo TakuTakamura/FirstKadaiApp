94f841c3ed52a7f66880329c9b39127c8859980d
taku.takamura <taku.takamura@techacademy.jp>
2019-09-24 13:53:48 +0900

    Initial Commit

---
+++ FirstKadaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaApp
+//
+//  Created by 高村拓 on 2019/09/24.
+//  Copyright © 2019 taku.takamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
